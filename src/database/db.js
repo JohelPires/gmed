a1cc00e946012013b8de84c6616370ced84ceb02
Johel Pires <103535247+JohelPires@users.noreply.github.com>
2024-02-21 20:58:53 -0400

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -6,16 +6,16 @@ const config = require('./config/config')
 //     host: 'localhost',
 // })
 
-// const sequelize = new Sequelize('gmed', 'postgres', '123', {
-//     dialect: 'postgres',
-//     host: 'localhost',
-// })
+const sequelize = new Sequelize('gmed', 'postgres', '123', {
+    dialect: 'postgres',
+    host: 'localhost',
+})
 
 // console.log(config.development)
 
-const sequelize = new Sequelize(
-    'postgres://kkyicuqc:W5h4zwgdGD6rQZa-_U-KfKnhOhBeJxLB@silly.db.elephantsql.com/kkyicuqc'
-)
+// const sequelize = new Sequelize(
+//     'postgres://kkyicuqc:W5h4zwgdGD6rQZa-_U-KfKnhOhBeJxLB@silly.db.elephantsql.com/kkyicuqc'
+// )
 
 // const sequelize = new Sequelize(
 //     'postgres://gmed:4CHc2naatlBTDVLQO4dMH8xYebhQgVkl@dpg-ck4adqnqj8ts73brtqbg-a.oregon-postgres.render.com/gmed'
